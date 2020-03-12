21ff46d906f6e7aca330c7c2824ce268b92d4e2a
SirTheta <sirtheta@gmail.com>
2020-03-12 18:44:40 +0100

    some changes

--- HangmanCS/MainWindow.xaml.cs
+++ HangmanCS/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.IO;
 using System.Windows.Media.Imaging;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace HangmanCS
 {
@@ -13,56 +14,43 @@ namespace HangmanCS
     {
         readonly string projectPath = Path.GetFullPath(@"..\..\..\");
         int counter = 0;
+        readonly List<string> images = new List<string>();
 
         public MainWindow()
         {
             InitializeComponent();
+            ImagesToList();
             SetImage();
         }
-        /*
-        private void RefreshNumberOfFailures()
+        
+        private void ImagesToList()
         {
-            List<string> images = new List<string>();
-            {
-                images.Add(":/resources/images/Hangman01.png");
-                images.Add(":/resources/images/Hangman02.png");
-                images.Add(":/resources/images/Hangman03.png");
-                images.Add(":/resources/images/Hangman04.png");
-                images.Add(":/resources/images/Hangman05.png");
-                images.Add(":/resources/images/Hangman06.png");
-                images.Add(":/resources/images/Hangman07.png");
-                images.Add(":/resources/images/Hangman08.png");
-                images.Add(":/resources/images/Hangman09.png");
-                images.Add(":/resources/images/Hangman10.png");
-                images.Add(":/resources/images/Hangman11.png");
-                images.Add(":/resources/images/Hangman12.png");
-            };*/
+            
+            images.Add("\\resources\\images\\Hangman01.png");
+            images.Add("\\resources\\images\\Hangman02.png");
+            images.Add("\\resources\\images\\Hangman03.png");
+            images.Add("\\resources\\images\\Hangman04.png");
+            images.Add("\\resources\\images\\Hangman05.png");
+            images.Add("\\resources\\images\\Hangman06.png");
+            images.Add("\\resources\\images\\Hangman07.png");
+            images.Add("\\resources\\images\\Hangman08.png");
+            images.Add("\\resources\\images\\Hangman09.png");
+            images.Add("\\resources\\images\\Hangman10.png");
+            images.Add("\\resources\\images\\Hangman11.png");
+            images.Add("\\resources\\images\\Hangman12.png");
+        }  
 
-   
-       
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             counter += 1;
             SetImage();         
             
         }
+
         private void SetImage()
         {
-            List<string> images = new List<string>();
-            {
-                images.Add("\\resources\\images\\Hangman01.png");
-                images.Add("\\resources\\images\\Hangman02.png");
-                images.Add("\\resources\\images\\Hangman03.png");
-                images.Add("\\resources\\images\\Hangman04.png");
-                images.Add("\\resources\\images\\Hangman05.png");
-                images.Add("\\resources\\images\\Hangman06.png");
-                images.Add("\\resources\\images\\Hangman07.png");
-                images.Add("\\resources\\images\\Hangman08.png");
-                images.Add("\\resources\\images\\Hangman09.png");
-                images.Add("\\resources\\images\\Hangman10.png");
-                images.Add("\\resources\\images\\Hangman11.png");
-                images.Add("\\resources\\images\\Hangman12.png");
-            }
+            Debug.WriteLine(counter, "counter");   
+            
             Uri fileUri = new Uri(projectPath + images[counter]);
             Background.Source = new BitmapImage(fileUri);
         }
